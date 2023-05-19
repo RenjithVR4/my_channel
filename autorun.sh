c610683a958acc26c9ec77b40f088aa0455267fd
RenjithVR4 <renjithvr.work@gmail.com>
2023-05-20 01:36:08 +0530

    updated list and concatinated files

--- autorun.sh
+++ autorun.sh
@@ -10,6 +10,6 @@ python3 youtube_m3ugrabber.py > ../youtube.m3u
 
 echo m3u grabbed
 
-cat ../youtube.m3u ../mal_family.m3u > ../list.m3u 
+cat ../youtube.m3u ../content/malayalam.m3u ../content/archive.m3u ../content/kids.m3u ../content/movies.m3u ../content/music.m3u ../content/nature.m3u ../content/other_news.m3u ../content/sports.m3u > ../list.m3u 
 
 
